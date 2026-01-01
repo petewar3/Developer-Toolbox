d6dc426c9311bc4ad55a4db12d9907385c07b59e
petewar3 <danieladams201178@gmail.com>
2026-01-01 18:42:47 +0000

    Create clonefunction.lua

---
+++ ExtraFunctions/clonefunction.lua
@@ -0,0 +1,21 @@
+return function(func)
+    if typeof(func) ~= "function" then
+        return nil
+    end
+    
+	local func_env = xpcall(setfenv, function(err, traceback)
+		return err, traceback
+	end, func, getfenv(func))
+	
+	if func_env then
+		return function(...)
+			return func(...)
+		end
+	end
+	
+	return coroutine.wrap(function(...)
+		while true do
+			func = coroutine.yield(func(...))
+		end
+	end)
+end
