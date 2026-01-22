36b3a90d317559504af938434802b0c1124cc9b3
petewar3 <danieladams201178@gmail.com>
2026-01-22 17:31:41 +0000

    Update getmetamethod.lua

--- Modules/getmetamethod.lua
+++ Modules/getmetamethod.lua
@@ -1,12 +1,13 @@
 local metamethods = loadstring(game:HttpGet("https://raw.githubusercontent.com/petewar3/Developer-Toolbox/refs/heads/main/Modules/metamethods.lua"))()
 
-return function(method)
-    local func = metamethods[method]
-    if not func then 
-        return nil 
-    end
+return function(instance, method)
+    assert(typeof(instance) == "Instance", string.format("bad argument #1 'getmetamethod' (Instance expected, got %s)", typeof(instance)))
+    assert(typeof(method) == "string", string.format("bad argument #2 'getmetamethod' (string expected, got %s)", typeof(method)))
+    
+    local metamethod = metamethods[method]
+    assert(typeof(metamethod) == "function", string.format("bad argument #2 'getmetamethod' (invalid metamethod: %s)", method))
 
-    local success, result = xpcall(func, function(err)
+    local success, result = xpcall(metamethod, function(err)
         return debug.info(2, "f")
     end)
 
