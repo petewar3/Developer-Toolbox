9fcc01957296599b34401530338d6a515ccb3138
petewar3 <danieladams201178@gmail.com>
2026-01-22 17:00:14 +0000

    Create metamethods.lua

---
+++ Modules/metamethods.lua
@@ -0,0 +1,50 @@
+return {
+    __index = function()
+        return game["_"]
+    end,
+    __newindex = function()
+        game["_"] = nil
+    end,
+    __namecall = function()
+        game:_()
+    end,
+    __call = function()
+        game()
+    end,
+    __len = function()
+        return #game
+    end,
+    __lt = function()
+        return game < num
+    end,
+    __le = function()
+        return game <= num
+    end,
+    __add = function()
+        game = game + num
+    end,
+    __sub = function()
+        game = game - num
+    end,
+    __mul = function()
+        game = game * num
+    end,
+    __div = function()
+        game = game / num
+    end,
+    __mod = function()
+        game = game % num
+    end,
+    __pow = function()
+        game = game ^ num
+    end,
+    __unm = function()
+        return -game
+    end,
+    __concat = function()
+        return game .. id
+    end,
+    __idiv = function()
+        game = game // num
+    end
+}
